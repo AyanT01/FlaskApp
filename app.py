1b54582b25f0475b95f41a1f6e83570567a90473
AyanT01 <abdulhaqayantayo@gmail.com>
2024-07-04 11:26:58 -0400

    Set up auto-deployment

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template 
+import git 
+from flask import Flask, render_template, url_for, flash, redirect, request
 
 app = Flask(__name__) 
 
@@ -6,5 +7,15 @@ app = Flask(__name__)
 def home():
     return render_template("home.html", name="Abdul Haq Ayantayo", school="Wayne State University")
 
+@app.route("/update_server", methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('AyanT/FlaskApp')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
+    
 if __name__ == "__main__":
     app.run(debug=True) 
